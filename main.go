974701bb6cb8cce2656126b34e7918686d5e908b
Fisherman August <fshrmn.august@gmail.com>
2025-12-23 20:41:46 +0300

    automigrate

--- main.go
+++ main.go
@@ -7,6 +7,15 @@ import (
 	"gorm.io/gorm"
 )
 
+type User struct {
+	gorm.Model
+	FirstName string `gorm:"uniqueIndex"`
+	LastName  string `gorm:"uniqueIndex"`
+	Email     string `gorm:"not null"`
+	Country   string `gorm:"not null"`
+	Age       int    `gorm:"not null;size:3"`
+}
+
 func main() {
 	pgConnect := "host=localhost user=postgres password=yd4v dbname=postgres port=5432"
 	db, err := gorm.Open(postgres.Open(pgConnect), &gorm.Config{})
@@ -14,4 +23,9 @@ func main() {
 		fmt.Println(err)
 		return
 	}
+	err = db.AutoMigrate(&User{})
+	if err != nil {
+		fmt.Println(err)
+	}
+
 }
